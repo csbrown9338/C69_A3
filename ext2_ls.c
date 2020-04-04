ea6f32d4dbc2ee1657198c9d7773fb44d299c3fa
Colleen Brown <csbrown9338@gmail.com>
2020-04-04 18:31:32 -0400

    ??

--- ext2_ls.c
+++ ext2_ls.c
@@ -50,6 +50,7 @@ int main(int argc, char **argv) {
             while (curr_pos < i->i_size && linkidx < links) {
                 //printf("confusion\n");
                 struct ext2_dir_entry_2 *e = get_dir_entry(disk, i, curr_block, curr_pos);
+                printf("namelength: %d, ", e->name_len);
                 if (strcmp(e->name, ".") == 0 || strcmp(e->name, "..") == 0) {
                     if (has_flag == 1) printf("%s\n", e->name);
                 }
