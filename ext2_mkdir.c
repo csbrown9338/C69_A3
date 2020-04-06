d9067ec91fc3f73bf4e4ba25de0ca79d6a77041b
Colleen Brown <csbrown9338@gmail.com>
2020-04-06 18:36:17 -0400

    aklsdjfhla

--- ext2_mkdir.c
+++ ext2_mkdir.c
@@ -28,7 +28,7 @@ int main(int argc, char **argv) {
     printf("raw name: %s, length: %d\n", raw_name, strlen(raw_name));
     char filename[strlen(raw_name)];
     strncpy(filename, raw_name, strlen(raw_name));
-    printf("raw name: %s, filename: %d\n", raw_name, filename);
+    printf("raw name: %s, filename: %s\n", raw_name, filename);
     char *parent = truncatePath(newdir);
     int inode = isValidDirectory(disk, parent);
     // Check that everything before the last '/' is a valid dir
