d394111e0fe7573c7b3b056a94715f9f78e85d56
Colleen Brown <csbrown9338@gmail.com>
2020-04-06 15:17:27 -0400

    sigh

--- ext2_helper.c
+++ ext2_helper.c
@@ -339,8 +339,10 @@ int allocateInode(unsigned char *disk, int size) {
     while (curr_block < dir->i_blocks) {
         unsigned int block = dir->i_block[curr_block];
         int curr_pos = 0;
-        printf("inode size: %d", dir->i_size);
+        printf("inode size: %d, ", dir->i_size);
         while (curr_pos < dir->i_size) {
+            printf("curr_pos: %d\n", curr_pos);
+            fflush(stdout)
             struct ext2_dir_entry_2 *e = get_dir_entry(disk, dir, curr_block, curr_pos);
             printf("looping through position\ninode: %d, name: %s\n", e->inode, e->name);
             fflush(stdout);
@@ -350,6 +352,8 @@ int allocateInode(unsigned char *disk, int size) {
         }
         curr_block++;
     }
+    printf("we're returning null, fam\n");
+    fflush(stdout)
     return NULL;
  }
 
