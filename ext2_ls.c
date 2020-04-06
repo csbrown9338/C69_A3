7de74dbfd9b80ab6286045b9a5a6185323a27cb5
Colleen Brown <csbrown9338@gmail.com>
2020-04-06 16:36:29 -0400

    uhhhh

--- ext2_ls.c
+++ ext2_ls.c
@@ -32,7 +32,8 @@ int main(int argc, char **argv) {
     inode = isValidDirectory(disk, path);
     int fileInode = isValidFile(disk, path);
     // If it's a directory... oh boi
-    
+    printf("bout to make some checks dude\n");
+    fflush(stdout);
     if (inode != -1) {
         // If there be the -a, do the . and ..
         int has_flag = 0; // 0 if no flag, 1 if -a flag
