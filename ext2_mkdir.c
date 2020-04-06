e9ddce7f9f76ae9b943f987cbe4570f1978614ca
Colleen Brown <csbrown9338@gmail.com>
2020-04-06 19:16:48 -0400

    oh

--- ext2_mkdir.c
+++ ext2_mkdir.c
@@ -26,9 +26,9 @@ int main(int argc, char **argv) {
     int exists_inode = isValidPath(disk, newdir);
     char *raw_name = extractFileName(newdir);
     printf("raw name: %s, length: %d\n", raw_name, strlen(raw_name));
-    char filename[strlen(raw_name)];
+    char filename[strlen(raw_name) + 1];
     strncpy(filename, raw_name, strlen(raw_name));
-    // filename[strlen(raw_name) + 1] = '\0';
+    filename[strlen(raw_name)] = '\0';
     printf("filename: %s, length: %d\n", filename, strlen(filename));
     char *parent = truncatePath(newdir);
     int inode = isValidDirectory(disk, parent);
